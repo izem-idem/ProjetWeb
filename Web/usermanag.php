cbc4d66ba1b0c91babd4dc98907bc28ae9600e0b
Lydoche <LydieTr27@gmail.com>
2021-12-03 16:01:35 +0100

    resultats avec lien vers les pages Genome et Prot

--- Web/usermanag.php
+++ Web/usermanag.php
@@ -29,34 +29,48 @@
     $res_user_name = pg_query($db_conn, $q_user_name) or die (pg_last_error());
     $res_user_array = pg_fetch_all_columns($res_user_name);
 
+    ?>
 
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
+    <?php
     foreach ($res_user_array as $id_user_name){
-      echo "<select id='sel".$id_user_name."'>
+      echo "<select name='sel".$id_user_name."'>
         <option value='Reader'> Reader</option>
         <option value='Annotator'> Annotator</option>
         <option value='Validator'> Validator</option>
         </select>
         <button class='little_submit_button' type='submit', name = 'submit".$id_user_name."'> Change role</button>";
-
-
-      if ($_POST["sel".$id_user_name] == 'Reader'){
-        $alter_role = "UPDATE website.users SET status = 'Reader' WHERE users.email = ".$id_user_name;
-      }
-      else if ($_POST["sel".$id_user_name] == 'Annotator') {
-        $alter_role = "UPDATE website.users SET status = 'Annotator' WHERE users.email = ".$id_user_name;
-      }
-      else if ($_POST["sel".$id_user_name] == 'Validator') {
-        $alter_role = "UPDATE website.users SET status = 'Validator' WHERE users.email = ".$id_user_name;
       }
 
-      echo "<button class='little_submit_button' type='submit', name = 'del".$id_user_name."'> Delete user</button>";
-      if ($_POST["del".$id_user_name]) {
-        $del_user = "DELETE FROM website.users
-        WHERE email = $id_user_name";
-      }
-    }
 
+
+      if (isset($_POST["submit".$id_user_name])) {
+
+        if ($_POST["sel".$id_user_name] == 'Reader'){
+          $alter_role = "UPDATE website.users SET status = 'Reader' WHERE users.email = ".$id_user_name;
+        }
+        else if ($_POST["sel".$id_user_name] == 'Annotator') {
+          $alter_role = "UPDATE website.users SET status = 'Annotator' WHERE users.email = ".$id_user_name;
+        }
+        else if ($_POST["sel".$id_user_name] == 'Validator') {
+          $alter_role = "UPDATE website.users SET status = 'Validator' WHERE users.email = ".$id_user_name;
+        }
+
+      }
+        echo "<button class='little_submit_button' type='submit', name = 'del".$id_user_name."'> Delete user</button>";
+        if ($_POST["del".$id_user_name]) {
+          $del_user = "DELETE FROM website.users
+          WHERE email = $id_user_name";
+        }
+
+
+    ?>
+  </form>
+
+    <?php
+
+    echo "</form>";
     while ($line = pg_fetch_array($res_user_name, null, PGSQL_ASSOC)) {
     echo "\t<tr>\n";
     foreach ($line as $col_value) {
